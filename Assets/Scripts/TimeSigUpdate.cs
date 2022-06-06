d6849c3bd66650fe0d5b71f293a7bd3002daf77b
jerrythemeatball@gmail.com <jerrythemeatball@gmail.com>
2022-06-06 12:54:41 -0400

    Changeset: 30 Added trails to the robot's wrists in the viewing page

---
+++ Assets/Scripts/TimeSigUpdate.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TimeSigUpdate : MonoBehaviour
+{
+    public timeSig thisTimeSig;
+    public TextMeshProUGUI textBox;
+
+    public enum timeSig
+    {
+        upperNumber,
+        lowerNumber
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
