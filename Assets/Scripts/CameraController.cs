e59727a9c6ad4a0206433c6fc0d380f6f190e106
jerrythemeatball@gmail.com <jerrythemeatball@gmail.com>
2022-07-02 14:07:25 -0400

    Changeset: 84 Added zoom functionality and the ability to turn the robot to the right or left

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -9,6 +9,7 @@ public class CameraController : MonoBehaviour
     GameObject grid;
     float radius = 3f; // Distance of the camera from the robot
     float angle = 3f*Mathf.PI/2f; // Set based on the relative position of the camera to the robot at the start of the secene
+    float cameraHeight = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -18,9 +19,71 @@ public class CameraController : MonoBehaviour
         grid = GameObject.Find("Axis Lines");
     }
 
-    public void Rotate90()
+    // Moves the camera closer to the robot
+    public void ZoomIn()
     {
+        radius = 1.25f;
+        cameraHeight = 1.5f;
+
+        // Updates the camera's position
+        camera.transform.position = new Vector3(Mathf.Cos(angle) * radius, cameraHeight, Mathf.Sin(angle) * radius + 3);
+
+    }
+
+    // Move the camera farther from the robot
+    public void ZoomOut()
+    {
+        radius = 3f;
+        cameraHeight = 1f;
+
+        // Updates the camera's position
+        camera.transform.position = new Vector3(Mathf.Cos(angle) * radius, cameraHeight, Mathf.Sin(angle) * radius + 3);
+    }
+
+    // Rotates the camera and grid 90 degrees counter clockwise
+    public void TurnLeft90()
+    {
+        // Updates the angle to be 90 degrees larger
         angle += Mathf.PI / 2;
+        
+        // Prevents angle from getting excessively large
+        if (angle == Mathf.PI * 2)
+            angle = 0;
+
+        // moves the camera in a circular motion
+        // Mathf.Cos and Sin use radians, Quaternion.Euler uses degrees
+        camera.transform.position = new Vector3(Mathf.Cos(angle) * radius, cameraHeight, Mathf.Sin(angle) * radius + 3);
+        camera.transform.rotation = Quaternion.Euler(0, -90, 0) * camera.transform.rotation;
+
+        // perfoms the same movement on the axis lines so they stay in the same location
+        grid.transform.position = new Vector3(Mathf.Cos(angle + Mathf.PI) * radius, cameraHeight, Mathf.Sin(angle + Mathf.PI) * radius + 3);
+        grid.transform.rotation = Quaternion.Euler(0, -90, 0) * grid.transform.rotation;
+    }
+
+    public void TurnRight90()
+    {
+        // Updates the angle to be 90 degrees smaller
+        angle -= Mathf.PI / 2;
+
+        // Prevents angle from getting excessively large
+        if (angle == 0)
+            angle = Mathf.PI * 2;
+
+        // moves the camera in a circular motion
+        // Mathf.Cos and Sin use radians, Quaternion.Euler uses degrees
+        camera.transform.position = new Vector3(Mathf.Cos(angle) * radius, cameraHeight, Mathf.Sin(angle) * radius + 3);
+        camera.transform.rotation = Quaternion.Euler(0, 90, 0) * camera.transform.rotation;
+
+        // perfoms the same movement on the axis lines so they stay in the same location
+        grid.transform.position = new Vector3(Mathf.Cos(angle + Mathf.PI) * radius, cameraHeight, Mathf.Sin(angle + Mathf.PI) * radius + 3);
+        grid.transform.rotation = Quaternion.Euler(0, 90, 0) * grid.transform.rotation;
+    }
+
+    public void RotateCustom(float degrees)
+    {
+        // Updates the angle, Mathf.Deg2Rad is used to modified from degrees to radians
+        angle += Mathf.Deg2Rad * degrees;
+
         // Prevents angle from getting excessively large
         if (angle == Mathf.PI * 2)
             angle = 0;
