008917d7fc109e698aa7b077ea82d08cccabdba6
connorcabrera@knights.ucf.edu <connorcabrera@knights.ucf.edu>
2022-08-01 18:11:33 -0400

    Changeset: 199

--- Assets/Scripts/ErrorAnalytics.cs
+++ Assets/Scripts/ErrorAnalytics.cs
@@ -22,6 +22,21 @@ public class ErrorAnalytics : MonoBehaviour
     void Start()
     {
         axisLines = GameObject.Find("Axis Lines");
+
+        Debug.Log("Test Case 1");
+        Debug.Log("LX = 3, LY = 5, RX = -2, RY = 4, AX = 0, AY = -1");
+        Debug.Log("Expected: 3, 6, 2, 5");
+        Debug.Log("Results: " + getLXVal(3f, 0f).ToString() + ", " + getLYVal(5f, -1f).ToString() + ", " + getRXVal(-2f, 0f).ToString() + ", " + getRYVal(4f, -1f).ToString());
+
+        Debug.Log("Test Case 2");
+        Debug.Log("LX = 3.33, LY = 5.55, RX = -2.22, RY = 4.44, AX = 0, AY = -1.11");
+        Debug.Log("Expected: 3.33, 6.66, 2.22, 5.55");
+        Debug.Log("Results: " + getLXVal(3.33f, 0f).ToString() + ", " + getLYVal(5.55f, -1.11f).ToString() + ", " + getRXVal(-2.22f, 0f).ToString() + ", " + getRYVal(4.44f, -1.11f).ToString());
+
+        Debug.Log("Test Case 3");
+        Debug.Log("LX = 2.37204, LY = -1.02671, RX = -1.81094, RY = 5.00623, AX = 3.40078, AY = 4.13643");
+        Debug.Log("Expected: 1.02874, 5.16314, 5.21172, 0.8698");
+        Debug.Log("Results: " + getLXVal(2.37204f, 3.40078f).ToString("n5") + ", " + getLYVal(-1.02671f, 4.13643f).ToString("n5") + ", " + getRXVal(-1.81094f, 3.40078f).ToString("n5") + ", " + getRYVal(5.00623f, 4.13643f).ToString("n5"));
     }
 
     // Toggle the display of the analytics pane
@@ -41,14 +56,34 @@ public class ErrorAnalytics : MonoBehaviour
     {
         if (!isShown) return;
 
-        leftXError = Mathf.Abs(leftHand.transform.position.x - axisLines.transform.position.x);
-        leftYError = Mathf.Abs(leftHand.transform.position.y - axisLines.transform.position.y);
-        rightXError = Mathf.Abs(rightHand.transform.position.x - axisLines.transform.position.x);
-        rightYError = Mathf.Abs(rightHand.transform.position.y - axisLines.transform.position.y);
+        leftXError = getLXVal(leftHand.transform.position.x, axisLines.transform.position.x);
+        leftYError = getLYVal(leftHand.transform.position.y, axisLines.transform.position.y);
+        rightXError = getRXVal(rightHand.transform.position.x, axisLines.transform.position.x);
+        rightYError = getRYVal(rightHand.transform.position.y, axisLines.transform.position.y);
 
         LHX.text = leftXError.ToString();
         LHY.text = leftYError.ToString();
         RHX.text = rightXError.ToString();
         RHY.text = rightYError.ToString();
+    }
+
+    public float getLXVal(float lx, float ax)
+    {
+        return Mathf.Abs(lx - ax);
+    }
+
+    public float getLYVal(float ly, float ay)
+    {
+        return Mathf.Abs(ly - ay);
+    }
+
+    public float getRXVal(float rx, float ax)
+    {
+        return Mathf.Abs(rx - ax);
+    }
+
+    public float getRYVal(float ry, float ay)
+    {
+        return Mathf.Abs(ry - ay);
     }
 }
