76b2ca11543449e47f601136a66fafa9770538b4
jmcmillan@knights.ucf.edu <jmcmillan@knights.ucf.edu>
2022-08-03 14:27:29 -0400

    Changeset: 208 Add trail controls to the recording screen - have them disabled by default

--- Assets/Scripts/TrailController.cs
+++ Assets/Scripts/TrailController.cs
@@ -10,26 +10,62 @@ public class TrailController : MonoBehaviour
     TrailRenderer trail;
     Slider trailSlider;
     Text label;
+    Toggle showTrails;
 
     float startingValue = 0.5f;
 
-    // Assign the listener for changing the slider length (which is based on time)
+    private bool isShown = false;
+
+    // Retrieve objects
+    void Awake()
+    {
+        trail = gameObject.GetComponent<TrailRenderer>(); // Grabs the trail renderer for this trail
+        trailSlider = GameObject.Find("Trail Length").GetComponent<Slider>(); // Grabs the trail slider by name
+        label = GameObject.Find("Length Label").GetComponent<Text>();
+        if (GameObject.Find("Toggle Trails"))
+        {
+            showTrails = GameObject.Find("Toggle Trails").GetComponent<Toggle>();
+        }
+    }
+
+    // Assign listeners and starting change values
     void Start()
     {
         // Assign variables from the world
-        trail = gameObject.GetComponent<TrailRenderer>(); // Grabs the trail renderer for this trail
-        trailSlider = GameObject.Find("Trail Length").GetComponent<Slider>(); // Grabs the trail slider by name
         trail.time = startingValue;
         trailSlider.value = startingValue;
 
-        label = GameObject.Find("Length Label").GetComponent<Text>();
         label.text = startingValue.ToString("0.0") + "s";
 
         // Adds listener that updates the trail length whenever the slider is updated
         trailSlider.onValueChanged.AddListener(delegate { ChangeTrailLength(trailSlider.value); });
+
+        // Gets the trail toggle, which exists only on the recording page
+        // Initailizes everything as hidden
+        if (showTrails)
+        {
+            trailSlider.gameObject.SetActive(false);
+            this.gameObject.SetActive(false);
+            showTrails.onValueChanged.AddListener(delegate { ToggleSliderState(); });
+        }
     }
 
-    
+    public void ToggleSliderState()
+    {
+        isShown = !isShown;
+
+        if (isShown)
+        {
+            this.gameObject.SetActive(true);
+            trailSlider.gameObject.SetActive(true);
+        }
+        else
+        {
+            this.gameObject.SetActive(false);
+            trailSlider.gameObject.SetActive(false);
+        }
+    }
+
     public void ChangeTrailLength(float newLength)
     {
         trail.time = newLength;
