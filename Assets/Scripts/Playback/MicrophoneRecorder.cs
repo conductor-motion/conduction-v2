4b40d42b5fb9c910bbf0306cbc9c118d94a50ef2
Frankn01 <noah.bradley.frank@gmail.com>
2023-04-04 18:06:03 -0400

    Added Toggles and fixed Time Sig

--- Assets/Scripts/Playback/MicrophoneRecorder.cs
+++ Assets/Scripts/Playback/MicrophoneRecorder.cs
@@ -5,11 +5,26 @@ using UnityEngine;
 // Microphone recording utility used to record microphone audio for evaluation purposes
 public class MicrophoneRecorder : MonoBehaviour
 {
+    bool play = true;
+    AudioSource audioSource;
     // Start the microhpone recording with a duration of 1000 (likely more than enough)
     void Start()
     {
-        AudioSource audioSource = GetComponent<AudioSource>();
+        audioSource = GetComponent<AudioSource>();
         audioSource.clip = Microphone.Start(null, true, 1000, 44100);
         audioSource.Play();
+    }
+
+    public void ToggleSound()
+    {
+        if (play)
+        {
+            audioSource.clip = Microphone.Start(null, true, 1000, 44100);
+            audioSource.Play();
+        }
+        else
+        {
+            audioSource.Stop();
+        }
     }
 }
